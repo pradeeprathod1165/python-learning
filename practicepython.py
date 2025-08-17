099ad188c34a47b7f8769031c0aa82d19347386a
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-08-17 23:21:02 +0530

    practice:conditions

--- practicepython.py
+++ practicepython.py
@@ -1,3 +1,4 @@
+#Variables and input/output
 #1. Create a program that asks the user for their name and age, then prints:
 #"Hello <name>, you are <age> years old."
 
@@ -5,3 +6,14 @@ name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 
 print(f'Hello {name}, you are {age} years old')
+
+# Conditions
+# Write a program that asks the user for a number.
+# If it’s even → print "Even number"
+# Else → print "Odd number"
+
+num1 = int(input("Enter a number: "))
+if num1 % 2 == 0:
+    print("even number ")
+else:
+    print("odd number")
