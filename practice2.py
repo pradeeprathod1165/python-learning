a40fa14a9c1c374cc4c481007ec3011848e5d8ae
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-08-19 23:55:56 +0530

    practice-day2:conditions

--- practice2.py
+++ practice2.py
@@ -1,3 +1,10 @@
 #1. Variables & Input
 name = input("Enter your name: ")
 print(f"Hello {name}, welcome to Python!")
+
+#2. Conditions
+num = int(input("Enter a number: "))
+if num > 0:
+    print("Positive")
+else:
+    print("Zero or Negative")
