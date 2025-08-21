0780351efd4f7b7089539f30776a0061c03ecc44
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-08-21 23:21:03 +0530

    practice:factorial of a number

--- practice3.py
+++ practice3.py
@@ -1,3 +1,10 @@
 #1. Reverse a String
 text = "Python"
 print("Reversed:", text[::-1])
+
+#2. Factorial of a Number
+num = 5
+fact = 1
+for i in range(1, num + 1):
+    fact *= i
+print("Factorial of", num, "is", fact)
