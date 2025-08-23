17b1b558215d2ad467cdbf3b80c43b27c42095db
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-08-23 23:54:26 +0530

    practice: simple calc

--- programs.py
+++ programs.py
@@ -16,3 +16,15 @@ if word == word[::-1]:
     print(f"{word} is a palindrome")
 else:
     print(f"{word} is not a palindrome")
+
+#3. Simple Calculator (with functions)
+# Simple Calculator
+def add(x, y): return x + y
+def sub(x, y): return x - y
+def mul(x, y): return x * y
+def div(x, y): return x / y if y != 0 else "undefined"
+
+print("Add:", add(5, 3))
+print("Subtract:", sub(5, 3))
+print("Multiply:", mul(5, 3))
+print("Divide:", div(5, 3))
