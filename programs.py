3f0e67c5c381927e9a0d91e41450360a252ac0da
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-08-23 23:53:57 +0530

    practice: palindrome

--- programs.py
+++ programs.py
@@ -7,3 +7,12 @@ print("Fibonacci series:")
 for _ in range(n):
     print(a, end=" ")
     a, b = b, a + b
+
+
+#2. Palindrome Checker
+# Check if a string is a palindrome
+word = "radar"
+if word == word[::-1]:
+    print(f"{word} is a palindrome")
+else:
+    print(f"{word} is not a palindrome")
