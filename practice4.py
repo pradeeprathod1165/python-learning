994789d87c58f0c10800c1f9fc61256eb186d3df
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-08-24 23:08:41 +0530

    practice: even or odd num

---
+++ practice4.py
@@ -0,0 +1,10 @@
+name = "Pradeep"
+age = 21
+print(f'Your name is {name}, and ypur age is {age}')
+
+#Check if a number is positive or negative
+num = int(input("Enter a number: "))
+if num %2 == 0:
+    print('even')
+else :
+    print('odd')
