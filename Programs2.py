5c4e7c20d83195fddc981d8b71145a5fe5e7ae84
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-08-25 23:36:32 +0530

    added: guessing number

---
+++ Programs2.py
@@ -0,0 +1,16 @@
+#guess the number
+import random
+
+secret = random.randint(1,20)
+
+while True:
+    guess = int(input('enter number to guess: '))
+
+    if guess == secret:
+        print('successful')
+        break
+    elif guess < secret:
+        print('too low')
+    else:
+        print('too high')
+    
