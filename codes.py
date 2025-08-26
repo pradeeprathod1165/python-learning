eba73c951eae72246be126f6e91e99455d3a663d
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-08-26 23:12:17 +0530

    calculator cofee

--- codes.py
+++ codes.py
@@ -24,3 +24,19 @@ import random
 coffees = ["Espresso", "Latte", "Cappuccino", "Mocha", "Americano"]
 
 print("Today's suggestion:", random.choice(coffees))
+
+
+menu = {"Espresso": 80, "Latte": 120, "Cappuccino": 150}
+total = 0
+
+while True:
+    order = input("Enter item (or type 'done' to finish): ")
+    if order.lower() == "done":
+        break
+    if order in menu:
+        total += menu[order]
+        print(f"Added {order} - ₹{menu[order]}")
+    else:
+        print("Item not available.")
+
+print("Your total bill is: ₹", total)
