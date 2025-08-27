71802678512f9a8fc6b97cbc001878947fef442f
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-08-27 23:29:05 +0530

    done

--- weatherapp.py
+++ weatherapp.py
@@ -11,3 +11,9 @@ url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&
 # --- FETCH DATA ---
 response = requests.get(url)
 data = response.json()
+
+# --- DISPLAY DATA ---
+print("City:", data["name"])
+print("Temperature:", data["main"]["temp"], "°C")
+print("Humidity:", data["main"]["humidity"], "%")
+print("Weather:", data["weather"][0]["description"])
