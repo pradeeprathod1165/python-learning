df2b017d0c165fe60ccf684b83c694a48b9148e7
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-08-27 23:28:14 +0530

    api url

--- weatherapp.py
+++ weatherapp.py
@@ -1,5 +1,9 @@
 import requests
 
 # --- SETTINGS ---
-city = "London"   # You can change this to any city
-api_key = "08722ea037ad1d34285fbd619ddef9d3"  # put your key here
+city = "London"  
+api_key = "08722ea037ad1d34285fbd619ddef9d3"  
+
+
+# --- API URL ---
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
