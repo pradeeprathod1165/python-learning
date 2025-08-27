634708ec6acac461bf3be706eb11b3aa03d0eb4d
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-08-27 23:28:40 +0530

    fetch data

--- weatherapp.py
+++ weatherapp.py
@@ -7,3 +7,7 @@ api_key = "08722ea037ad1d34285fbd619ddef9d3"
 
 # --- API URL ---
 url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+
+# --- FETCH DATA ---
+response = requests.get(url)
+data = response.json()
