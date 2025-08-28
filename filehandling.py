c4414c487638dee779097651f0aa4109473a14c0
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-08-28 21:54:58 +0530

    learning: write file

--- filehandling.py
+++ filehandling.py
@@ -3,3 +3,8 @@
 #Open file
 file = open('example.txt','w')
 
+#write file
+f = open("example.txt", "w")  
+f.write("Hello, Python!\n")  
+f.write("This is file handling step by step.")  
+f.close()  # always close the file
