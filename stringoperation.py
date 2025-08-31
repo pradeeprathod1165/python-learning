23375211ab4eca78b9984686facc814e3f4fac21
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-08-31 23:30:28 +0530

    learning: string operation - changing case

--- stringoperation.py
+++ stringoperation.py
@@ -9,3 +9,11 @@ print(text[0:4])   # Pyth  (from 0 to 3)
 print(text[:3])    # Pyt   (from start to 2)
 print(text[2:])    # thon  (from 2 to end)
 print(text[::-1])  # nohtyP (reverse string)
+
+#3. Changing Case
+text = "pYtHoN"
+print(text.upper())   # PYTHON
+print(text.lower())   # python
+print(text.title())   # Python
+print(text.capitalize())  # Python
+print(text.swapcase())    # PyThOn
