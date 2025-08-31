7991450e836535971e3bbe33b59ac1dc82ae96aa
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-08-31 23:29:58 +0530

    learning: string operation - slicing

--- stringoperation.py
+++ stringoperation.py
@@ -2,3 +2,10 @@
 text = "Python"
 print(text[0])   # P  (first char)
 print(text[-1])  # n  (last char)
+
+#2. Slicing
+text = "Python"
+print(text[0:4])   # Pyth  (from 0 to 3)
+print(text[:3])    # Pyt   (from start to 2)
+print(text[2:])    # thon  (from 2 to end)
+print(text[::-1])  # nohtyP (reverse string)
