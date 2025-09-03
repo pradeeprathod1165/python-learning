00477181e8ab4335fd913ea4118fcca51a317044
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-03 23:58:07 +0530

    practice: multiplication table

--- realpractice.py
+++ realpractice.py
@@ -45,3 +45,9 @@ for i in range(1,20):
 num = input('enter num:')
 for i in range(1,10):
    print(num*i)
+
+#multiplication 
+num = int(input("Enter a number: "))
+
+for i in range(1, 11):   # 1 to 10
+    print(num, "x", i, "=", num * i)
