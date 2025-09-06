1e2bf6a609f888af00559518acf61719e383f8c5
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-06 23:42:12 +0530

    practice: fucntions

--- realpractice.py
+++ realpractice.py
@@ -51,3 +51,9 @@ num = int(input("Enter a number: "))
 
 for i in range(1, 11):   # 1 to 10
     print(num, "x", i, "=", num * i)
+
+
+
+
+      
+
---
+++ realpractice2.py
@@ -0,0 +1,6 @@
+#functions
+def greet(name):
+    print('Hello',name)
+
+#calling function
+greet('Pradeep')
