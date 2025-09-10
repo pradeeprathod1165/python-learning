591b8a2fda18e939e73dda460fcee1c11c88d2ca
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-10 22:36:37 +0530

    update: display

--- hangman.py
+++ hangman.py
@@ -4,3 +4,13 @@ word_list = ['python', 'hangman', 'challenge', 'programming', 'github']
 
 chosen_word = random.choice(word_list)
 print("The secret word (for now):", chosen_word)  
+
+#display
+display = []
+for letter in chosen_word:
+    display.append('_')
+
+print("Welcome to Hangman!")
+print(" ".join(display))
+
+
