4ba8b1596d9aa3328b9ba2647ed0dae25b1b5ba7
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-10 22:34:10 +0530

    build: hangman game

---
+++ hangman.py
@@ -0,0 +1,6 @@
+import random
+
+word_list = ['python', 'hangman', 'challenge', 'programming', 'github']
+
+chosen_word = random.choice(word_list)
+print("The secret word (for now):", chosen_word)  
