9e24d9e953d4ba44e2632fed38a60f2abfbc66bf
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-10 22:44:17 +0530

    update: final

--- hangman.py
+++ hangman.py
@@ -3,19 +3,34 @@ import random
 word_list = ['python', 'hangman', 'challenge', 'programming', 'github']
 chosen_word = random.choice(word_list)
 
-display = []
-for letter in chosen_word:
-    display.append('_')
+display = ['_'] * len(chosen_word)
+lives = 6  # Number of allowed wrong guesses
+guessed_letters = set()
 
 print("Welcome to Hangman!")
 
-while '_' in display:
-    print(" ".join(display))
+while '_' in display and lives > 0:
+    print("\nWord:", " ".join(display))
+    print(f"Lives left: {lives}")
+    print(f"Guessed letters: {', '.join(sorted(guessed_letters)) if guessed_letters else 'None'}")
     guess = input("Guess a letter: ").lower()
 
-    # Reveal the guessed letter in the correct position(s)
-    for position in range(len(chosen_word)):
-        if chosen_word[position] == guess:
-            display[position] = guess
+    if guess in guessed_letters:
+        print("You've already guessed that letter. Try again.")
+        continue
 
-print("Congratulations! You guessed the word:", chosen_word)
+    guessed_letters.add(guess)
+
+    if guess in chosen_word:
+        for position in range(len(chosen_word)):
+            if chosen_word[position] == guess:
+                display[position] = guess
+        print(f"Good guess! '{guess}' is in the word.")
+    else:
+        lives -= 1
+        print(f"Wrong! '{guess}' is not in the word.")
+
+if '_' not in display:
+    print(f"\nCongratulations! You guessed the word: {chosen_word}")
+else:
+    print(f"\nGame over! The word was: {chosen_word}")
