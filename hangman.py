5a235177ecdaeb4525e7f7fb492449a5f6090af6
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-10 22:43:08 +0530

    update: reveal

--- hangman.py
+++ hangman.py
@@ -1,24 +1,21 @@
 import random
 
 word_list = ['python', 'hangman', 'challenge', 'programming', 'github']
-
 chosen_word = random.choice(word_list)
-print("The secret word (for now):", chosen_word)  
 
-#display
 display = []
 for letter in chosen_word:
     display.append('_')
 
 print("Welcome to Hangman!")
-print(" ".join(display))
 
-#guess
-guess = input("Guess a letter: ").lower()
+while '_' in display:
+    print(" ".join(display))
+    guess = input("Guess a letter: ").lower()
 
-# Reveal the guessed letter in the correct position(s)
-for position in range(len(chosen_word)):
-    if chosen_word[position] == guess:
-        display[position] = guess
+    # Reveal the guessed letter in the correct position(s)
+    for position in range(len(chosen_word)):
+        if chosen_word[position] == guess:
+            display[position] = guess
 
-print(" ".join(display))
+print("Congratulations! You guessed the word:", chosen_word)
