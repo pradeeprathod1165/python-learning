6df8743a40a28d0ac9c839f130a2d76973a1e4ba
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-13 22:42:59 +0530

    program:grade calculator

---
+++ sep13.py
@@ -0,0 +1,9 @@
+#grade calculator
+sub1 = int(input('Enter mark sub1:'))
+sub2 = int(input('Enter mark sub2:'))
+sub3 = int(input('Enter mark sub3:'))
+
+#average 
+sum = sub1 + sub2 + sub3 
+average = (sum / 3)
+print(average)
