30e3e79326a5b8e832f931bf6909f04ff67e43e0
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-16 00:00:57 +0530

    program : pattern

--- realpractice2.py
+++ realpractice2.py
@@ -56,3 +56,9 @@ for i in range(1, rows+1):
         print("*", end=" ")
     print()
 
+#pattern
+rows = 5
+for i in range(1, rows+1):
+    for j in range(i):
+        print("*", end=" ")
+    print()
