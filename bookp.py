b59c646160e3d4b094e24fea7df1238f42474c80
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-17 23:19:41 +0530

    program: add book operation done

--- bookp.py
+++ bookp.py
@@ -8,3 +8,13 @@ while True:
   print('3. Search Book')
   print('4. Remove Book')
   print('5. Exit')
+
+
+  choice = int(input('enter option: '))
+
+  if choice == 1:
+        bookname = input('enter book name:')
+        author = input('enter author name:')
+        bookdetail = {"title": bookname, "author": author}
+        books.append(bookdetail)
+        print('book added successfull')
