95872f273a2a5f5354b00526bf4f278f5694bc09
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-17 23:12:14 +0530

    program: book library

---
+++ bookp.py
@@ -0,0 +1,10 @@
+#menu
+books = []
+
+while True:
+  print( ' --- Library Menu ---')
+  print('1. Add Book')
+  print('2. View All Books')
+  print('3. Search Book')
+  print('4. Remove Book')
+  print('5. Exit')
