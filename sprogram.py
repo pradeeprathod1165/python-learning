7ae60a16058f425c894f99d6f557de3d5690aae1
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-18 23:45:48 +0530

    program: password strength

--- sprogram.py
+++ sprogram.py
@@ -13,3 +13,18 @@ while True:
     current_time = time.strftime("%H:%M:%S")
     print("⏰ Time:", current_time, end="\r")  # overwrite same line
     time.sleep(1)
+
+
+#done
+    password = input("Enter your password: ")
+    
+    length = len(password)
+    has_digit = any(ch.isdigit() for ch in password)
+    has_special = any(not ch.isalnum() for ch in password)
+    
+    if length < 6:
+        print("Weak password ❌")
+    elif length >= 6 and has_digit and has_special:
+        print("Strong password 💪")
+    else:
+        print("Medium strength ⚡")
