0a155a2c837e15167f4244629574c1d8a623bde5
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-18 23:43:16 +0530

    program: clock

--- sprogram.py
+++ sprogram.py
@@ -5,3 +5,11 @@ print("Fibonacci sequence:")
 for _ in range(n):
     print(a, end=" ")
     a, b = b, a + b
+
+#clock
+import time
+
+while True:
+    current_time = time.strftime("%H:%M:%S")
+    print("⏰ Time:", current_time, end="\r")  # overwrite same line
+    time.sleep(1)
