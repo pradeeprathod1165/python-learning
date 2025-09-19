24d84f5d60779c9dfa6529b692b80c5a93b110ae
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-19 23:21:46 +0530

    program: menudriven add operation done

--- menudriven.py
+++ menudriven.py
@@ -9,3 +9,9 @@ while True:
     print("7. exit")
 
     choice = int(input('enter option: '))
+
+    if choice == 1:
+        num1 = int(input('enter number1:')) 
+        num2 = int(input('enter number2:')) 
+
+        print("addition is:", num1 + num2)
