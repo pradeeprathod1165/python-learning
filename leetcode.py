37df795d2f6615f3f31bf96f95341b368ede4251
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-20 23:19:24 +0530

    leetcode: 9-palindrome

---
+++ leetcode.py
@@ -0,0 +1,10 @@
+#9 - palindrome
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        if x < 0:
+            return False
+
+       
+        s = str(x)
+        return s == s[::-1]
