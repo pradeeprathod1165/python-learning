44cb14fb74ad15bbfce5acd5ae56a73140e2257a
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-20 23:36:18 +0530

    leetcode: 412-fizzbuzz

--- leetcode.py
+++ leetcode.py
@@ -8,3 +8,19 @@ class Solution:
        
         s = str(x)
         return s == s[::-1]
+
+
+#412. Fizz Buzz
+class Solution:
+    def fizzBuzz(self, n: int):
+        result = []
+        for i in range(1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                result.append("FizzBuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+        return result
