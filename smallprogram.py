915dcb0e761e9537d2f764d8b99f7475fa5cdc96
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-21 23:56:02 +0530

    leetcode: classes

--- smallprogram.py
+++ smallprogram.py
@@ -3,3 +3,12 @@ def check(n):
     return "Even" if n % 2 == 0 else "Odd"
 
 print(check(int(input("Enter number: "))))
+
+
+#Small Class Example
+class Person:
+    def __init__(self, name): self.name = name
+    def greet(self): print("Hello,", self.name)
+
+p = Person(input("Enter name: "))
+p.greet()
