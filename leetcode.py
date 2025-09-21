0cbd46a14e849d09c39bdee89e6c0fff2903bbee
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-21 23:54:23 +0530

    leetcode: 1-two sum

--- leetcode.py
+++ leetcode.py
@@ -24,3 +24,12 @@ class Solution:
             else:
                 result.append(str(i))
         return result
+    
+#1 -two sum
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                 return [i,j]
---
+++ smallprogram.py
@@ -0,0 +1,5 @@
+#Check Even or Odd
+def check(n):
+    return "Even" if n % 2 == 0 else "Odd"
+
+print(check(int(input("Enter number: "))))
