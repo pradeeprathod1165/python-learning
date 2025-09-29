31188d92e5fcfa179ced91e44b9f5d68d69c4c6e
pradeeprathod1165 <rishirathod1165@gmail.com>
2025-09-29 23:26:59 +0530

    program: Get your grade

--- pcode.py
+++ pcode.py
@@ -10,3 +10,15 @@ total = sum(nlist)
 greater = max(nlist)
 small = min(nlist)
 print('sum:',total ,'greater number:' ,greater,'smaller number:', small)
+
+#get grade
+student = int(input('enter your percentage(e.g- 70):'))
+
+if student >= 60 :
+    print('you got a grade:A')
+elif student < 60 and student >40 :
+    print('you got a grade:B')
+if student >=35 and student == 40 :
+    print('you got a grade:c')
+else:
+    print('sorry you are fail and got grade:f')
